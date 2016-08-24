edfc3b08a12c36bcbd93ca5d9837a0383c829d46
Dariusz Pobożniak <dariusz.pobozniak@performgroup.com>
2016-08-24 11:54:36 +0200

    Remove unecessary code

--- src/clientRender.js
+++ src/clientRender.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
-// import createBrowserHistory from 'history/lib/createBrowserHistory';
-
-// const history = createBrowserHistory();
 
 import routes from './routes';
 
