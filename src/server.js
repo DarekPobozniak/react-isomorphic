e561f05ceafe936bd228652cff887b00ec4968df
Dariusz Pobożniak <dariusz.pobozniak@performgroup.com>
2016-08-24 11:54:09 +0200

    Allow changing the port

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { match, RouterContext } from 'react-router';
 import routes from './routes';
 
 const app = express();
+const port = process.env.PORT || 3003;
 
 app.use(express.static('public'));
 
@@ -16,17 +17,12 @@ app.set('view engine', 'ejs');
 app.get('*', (req, res) => {
   match({ routes, location: req.url }, (err, redirectLocation, props) => {
     if (err) {
-      // something went badly wrong, so 500 with a message
       res.status(500).send(err.message);
     } else if (redirectLocation) {
-      // we matched a ReactRouter redirect, so redirect from the server
       res.status(302, redirectLocation.pathname + redirectLocation.search);
     } else if (props) {
-      // if we got props, that means we found a valid component to render
-      // for the given route
       const markup = renderToString(<RouterContext {...props} />);
 
-      // render `index.ejs`, but pass in the markup we want it to display
       res.render('index', { markup });
     } else {
       res.sendStatus(404);
@@ -36,11 +32,11 @@ app.get('*', (req, res) => {
 
 const server = http.createServer(app);
 
-app.listen(3003, 'localhost', function(err) {
+app.listen(port, function(err) {
   if (err) {
     console.log(err);
     return;
   }
 
-  console.log('Listening at http://localhost:3003');
+  console.log(`Listening at http://localhost:${port}`);
 });
